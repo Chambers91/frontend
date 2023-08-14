729ca6c7fa297faf4dfaa9cd1cc2704b99fbcb96
Terence Chambers <terencechambers91@gmail.com>
2023-08-14 16:16:11 -0400

    new components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthContentComponent } from './auth-content/auth-content.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthContentComponent
+    AuthContentComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
